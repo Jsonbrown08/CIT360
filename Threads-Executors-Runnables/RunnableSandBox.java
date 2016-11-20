bfdacc49ba6419e5d269eb3a4792fb69b5dcd204
Jsonbrown08 <Json.brown08@gmail.com>
2016-11-19 18:21:12 -0700

    RunnableSandBox

---
+++ Threads-Executors-Runnables/RunnableSandBox.java
@@ -0,0 +1,23 @@
+
+public class RunnableSandBox implements Runnable {
+	private int counter = 1;
+	public void run() {
+		synchronized(this)
+		{
+			try {
+				for (int i = 1; i < 1 ; i++) {
+					System.out.println("Thread ID: " + Thread.currentThread().getName() +
+							" Counter: " + counter++);
+					Thread.sleep(100);
+				}
+			}
+			catch(InterruptedException e) {
+				System.out.println(e);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+	}
+
+}
